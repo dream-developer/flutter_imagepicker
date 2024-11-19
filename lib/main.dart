aed441d40f88654e7a3a324635b20f12eba52edc
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:46:37 +0900

    イメージピッカー - ギャラリーへ画像を保存する

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'dart:io'; 
 import 'package:image_picker/image_picker.dart';
 
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'dart:typed_data'; 
+import 'package:fluttertoast/fluttertoast.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -44,6 +48,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future _savePhoto() async { // ギャラリーへ画像を保存する関数
+    if (_photo != null) {
+      Uint8List buffer = await _photo!.readAsBytes(); // 1
+      final result = await ImageGallerySaver.saveImage(buffer); // 2
+      print(result); // デバッグ用
+      Fluttertoast.showToast(msg: "写真を保存しました");
+    }else{
+      Fluttertoast.showToast(msg: "写真を撮影してください");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final photoView = SizedBox( // 写真撮影のビュー
@@ -69,6 +84,11 @@ class _MyHomePageState extends State<MyHomePage> {
         FloatingActionButton(
           onPressed: _takePhoto, // 写真撮影をする関数
           child: const Icon(Icons.photo_camera),
+        ),
+        const SizedBox(width: 20), 
+        FloatingActionButton( // ギャラリーへ画像を保存する関数
+          onPressed: _savePhoto,
+          child: const Icon(Icons.save,),
         ),
       ]
     );
